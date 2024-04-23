dae090d86c299e07af47830abbc24e9c2599458f
redfoxbg <redfoxbg@gmail.com>
2024-04-23 21:03:37 +0300

    Setup canvas

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const GameViewport = {
     WIDTH: 384,
     HEIGHT: 224,
-    SCALE: 4
 };
 
 window.onload = function(){
@@ -11,8 +10,12 @@ window.onload = function(){
     canvasEL.width = GameViewport.WIDTH;
     canvasEL.height = GameViewport.HEIGHT;
 
-    canvasEL.style.width = `${GameViewport.WIDTH * GameViewport.SCALE}px`;
-    canvasEL.style.height = `${GameViewport.HEIGHT * GameViewport.SCALE}px`;
+    context.strokeStyle = 'yellow';
+    context.moveTo(0, 0);
+    context.lineTo(GameViewport.WIDTH, GameViewport.HEIGHT);
+    context.moveTo(GameViewport.WIDTH, 0);
+    context.lineTo(0, GameViewport.HEIGHT);
+    context.stroke();
 
     console.log(context);
 }
