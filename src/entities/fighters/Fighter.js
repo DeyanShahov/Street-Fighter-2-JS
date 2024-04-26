d6927b0684880210f563206942cc6baf7c2c8432
redfoxbg <redfoxbg@gmail.com>
2024-04-27 01:47:49 +0300

    Use origin points to animate correctly

--- src/entities/fighters/Fighter.js
+++ src/entities/fighters/Fighter.js
@@ -5,25 +5,61 @@ export class Fighter {
         this.frames = new Map();
         this.position = {x, y};
         this.velocity = velocity;
-        this.animationFrame = 1;
+        this.animationFrame = 0;
+        this.animationTimer = 0;
+        this.state = 'walkForwards';
+        this.animations = {};
     }
 
-    update(secondsPassed, context) {
-        const [, , width] = this.frames.get(`forwards-${this.animationFrame}`);
+    update(time, context) {
+        const [[, , width]] = this.frames.get(this.animations[this.state][this.animationFrame]);
 
-        this.animationFrame++;
-        if (this.animationFrame > 6) this.animationFrame = 1;
+        if (time.previous > this.animationTimer + 60) {
+            this.animationTimer = time.previous;
+
+            this.animationFrame++;
+            if (this.animationFrame > 5) this.animationFrame = 0;
+        };
+
+        this.position.x += this.velocity * time.secondsPassed;
         
-        this.position.x += this.velocity * secondsPassed;
-        
-        if(this.position.x > context.canvas.width - width || this.position.x < 0) {
-            this.velocity = -this.velocity;
-        }
+        if(this.position.x > context.canvas.width - width / 2) {
+            this.velocity = -150;
+            this.state = 'walkBackwards';
+        };
+
+        if(this.position.x < width /2 ) {
+            this.velocity = 150;
+            this.state = 'walkForwards';
+        };
+    }
+
+    drawDebug(context) {
+        context.lineWidth = 1;
+
+        context.beginPath();
+        context.strokeStyle = 'white';
+        context.moveTo(this.position.x - 5, this.position.y);
+        context.lineTo(this.position.x + 4, this.position.y);
+        context.moveTo(this.position.x, this.position.y - 5);
+        context.lineTo(this.position.x, this.position.y + 4);
+        context.stroke();
     }
 
     draw(context) {
-        const [x, y, width, height] = this.frames.get(`forwards-${this.animationFrame}`);
+        const [
+            [x, y, width, height],
+            [originX, originY],
+        ] = this.frames.get(this.animations[this.state][this.animationFrame]);
 
-        context.drawImage(this.image, x, y, width, height, this.position.x, this.position.y, width, height);
+        context.drawImage(
+            this.image,
+            x, y,
+            width, height,
+            this.position.x - originX, this.position.y - originY,
+            width, height
+        );
+
+        this.drawDebug(context);
     }
 }
