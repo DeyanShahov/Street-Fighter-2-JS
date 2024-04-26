d2a05d0a7a0fa0ece1c456816ccae23c6a42d04d
redfoxbg <redfoxbg@gmail.com>
2024-04-26 17:07:23 +0300

    Extended drawimage functions, extract frames from sprite sheet

--- src/entities/fighters/Ken.js
+++ src/entities/fighters/Ken.js
@@ -5,5 +5,14 @@ export class Ken extends Fighter {
         super('Ken', x, y, velosity);
 
         this.image = document.querySelector('img[alt="Ken"]');
+
+        this.frames = new Map([
+            ['forwards-1', [882, 745, 53, 83]],
+            ['forwards-2', [874, 902, 60, 88]],
+            ['forwards-3', [793, 901, 64, 89]],
+            ['forwards-4', [721, 900, 63, 89]],
+            ['forwards-5', [641, 901, 54, 89]],
+            ['forwards-6', [576, 902, 50, 89]]
+        ]);
     }
 }
