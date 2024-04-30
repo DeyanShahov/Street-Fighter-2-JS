9539e10d8e175c079c6e98397144d358f3cd82ee
redfoxbg <redfoxbg@gmail.com>
2024-04-30 04:19:00 +0300

    Refactor Index.js file in class for clean code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import { Ken } from './entities/fighters/Ken.js';
-import { Ryu } from './entities/fighters/Ryu.js';
-import { Stage } from './entities/Stage.js';
-import { FpsCounter } from './entities/FpsCounter.js'
-import { STAGE_FLOOR } from './constants/stage.js';
-import { FighterDirection, FighterState } from './constants/fighters.js';
-
-// const GameViewport = {
-//     WIDTH: 384,
-//     HEIGHT: 224,
-// };
+import { StreetFighterGame } from './StreetFighterGame.js';
+import { FighterState } from './constants/fighters.js';
 
 function populateMoveDropdown() {
     const dropdown = document.getElementById('state-dropdown');
@@ -21,67 +12,9 @@ function populateMoveDropdown() {
     }); 
 }
 
-function handleFormSubmit(event, fighters) {
-    event.preventDefault();
-
-    const selectedCheckboxes = Array
-        .from(event.target.querySelectorAll('input:checked'))
-        .map(checkbox => checkbox.value);
-
-    const options = event.target.querySelector('select');
-
-    fighters.forEach(fighter => {
-        if(selectedCheckboxes.includes(fighter.name)) {
-            fighter.changeState(options.value);
-        }
-    });
-}
 
 window.addEventListener('load', function() {
     populateMoveDropdown();
 
-    const canvasEL = document.querySelector('canvas');
-    const context = canvasEL.getContext('2d');
-
-    context.imageSmoothingEnabled = false;
-
-    // canvasEL.width = GameViewport.WIDTH;
-    // canvasEL.height = GameViewport.HEIGHT;
-
-    const fighters = [
-        new Ryu(180, STAGE_FLOOR, FighterDirection.RIGHT),
-        new Ken(280, STAGE_FLOOR, FighterDirection.LEFT),
-    ]
-
-    const entities = [
-        new Stage(), 
-        ...fighters, 
-        new FpsCounter()
-    ];
-
-    let frameTime = {
-        previous: 0,
-        secondsPassed: 0,
-    };
-
-    function frame(time) {
-        window.requestAnimationFrame(frame);
-
-        frameTime = {
-            secondsPassed: ( time - frameTime.previous) / 1000,
-            previous: time,
-        }
-
-        for (const entity of entities) {
-            entity.update(frameTime, context);
-        }
-
-        for (const entity of entities) {
-            entity.draw(context);
-        }
-    }
-
-    this.document.addEventListener('submit', (event) => handleFormSubmit(event, fighters));
-
-    window.requestAnimationFrame(frame);
+    new StreetFighterGame().start();
 });
